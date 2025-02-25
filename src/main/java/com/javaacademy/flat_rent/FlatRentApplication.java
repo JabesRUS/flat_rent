90bda1b5cf42f35153f9cb04fed9f986914a0859
jabes <jabesgames@gmail.com>
2025-02-25 10:20:57 +0300

    init commit

---
+++ src/main/java/com/javaacademy/flat_rent/FlatRentApplication.java
@@ -0,0 +1,13 @@
+package com.javaacademy.flat_rent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlatRentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlatRentApplication.class, args);
+	}
+
+}
