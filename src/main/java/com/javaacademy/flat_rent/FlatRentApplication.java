5b47b1e8b1ac9a755b660fe71d26cb598c598c3e
jabes <jabesgames@gmail.com>
2025-03-05 11:04:09 +0300

    Правки 2 этап

--- src/main/java/com/javaacademy/flat_rent/FlatRentApplication.java
+++ src/main/java/com/javaacademy/flat_rent/FlatRentApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FlatRentApplication {
 
-    private static final int DAYS_TO_ADD = 7;
-
     public static void main(String[] args) {
         SpringApplication.run(FlatRentApplication.class, args);
     }
