31084599f07204ce3db6600a2b9861a64d8cd353
jabes <jabesgames@gmail.com>
2025-03-05 10:34:00 +0300

    Правки 2 этап

--- src/main/java/com/javaacademy/flat_rent/FlatRentApplication.java
+++ src/main/java/com/javaacademy/flat_rent/FlatRentApplication.java
@@ -1,22 +1,14 @@
 package com.javaacademy.flat_rent;
 
-import com.javaacademy.flat_rent.entity.Booking;
-import com.javaacademy.flat_rent.repository.AdvertRepository;
-import com.javaacademy.flat_rent.repository.ApartmentRepository;
-import com.javaacademy.flat_rent.repository.BookingRepository;
-import com.javaacademy.flat_rent.repository.ClientRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-
-import java.math.BigDecimal;
-import java.time.LocalDate;
 
 @SpringBootApplication
 public class FlatRentApplication {
 
     private static final int DAYS_TO_ADD = 7;
 
-    public static void main(String[] args) {SpringApplication.run(FlatRentApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(FlatRentApplication.class, args);
     }
 }
